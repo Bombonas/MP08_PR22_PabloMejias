081e9d2cc0ab52d62cbf58952f79ddbca22f5c91
Bombonas <pablomejiast@gmail.com>
2023-12-12 18:10:48 +0100

    commit error FileProvider

--- app/src/main/java/com/example/pr22_pablomejias/MainActivity.java
+++ app/src/main/java/com/example/pr22_pablomejias/MainActivity.java
@@ -5,24 +5,33 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     public static int RC_PHOTO_PICKER = 0;
     ActivityResultLauncher<Intent> someActivityResultLauncher;
     ActivityResultLauncher<Intent> someActivityResultLauncher2;
-    Button button, button2;
 
+    ActivityResultLauncher<Intent> someActivityResultLauncher3;
+    Button button, button2;
+    String currentPhotoPath;
     ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +68,28 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
+
+        someActivityResultLauncher3 = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            File photoFile = null;
+                            try {
+                                photoFile = createImageFile();
+                            } catch (IOException ex) {
+                            }
+                            if (photoFile != null) {
+                                Uri photoURI = FileProvider.getUriForFile(MainActivity.this,
+                                        "com.example.pr22_pablomejias",
+                                        photoFile);
+                                data.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                            }
+                        }
+                    }
+                });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,8 +104,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                someActivityResultLauncher2.launch(intent);
+                someActivityResultLauncher3.launch(intent);
             }
         });
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
